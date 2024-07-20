17b8b4d6161ee28c034649113b350bc554e0ee73
ragulnath255 <ragulnath.franchizerz@gmail.com>
2024-07-20 16:49:51 +0530

    Updated Franchise.jsx
    added Menu and FanchiseGallery components

--- src/components/Franchise/Franchise.jsx
+++ src/components/Franchise/Franchise.jsx
@@ -2,12 +2,16 @@ import FranchiseData from "../FranchiseData/FranchiseData"
 import WhyFranchizerz from "../WhyFranchizerz/WhyFranchizerz"
 import FranchiseModels from "../FranchiseModels/FranchiseModels"
 import Reviews from "../Reviews/Reviews"
+import Menu from "../MenuCard/Menu"
+import FranchiseGallery from "../BrandGallery/franchiseGallery"
 
 const Franchise = ({ franchise }) => (
 	<>
 		<FranchiseData franchise={franchise} />
 		<WhyFranchizerz />
 		<FranchiseModels />
+		<Menu menuPhotos={franchise.menuPhotos}/>
+		<FranchiseGallery galleryPhotos={franchise.brandGalleryPhotos}/>
 		<Reviews reviews={franchise.reviews} />
 	</>
 )
