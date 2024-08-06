fd659fce1030e706e0b3e0115fc62d21e85e9c59
SaurabhPandey9752 <saurabhlesner9752@gmail.com>
2024-08-06 23:07:19 +0530

    Update Navbar styles for responsive screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,72 +1,80 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./Navbar.css"
-import HomeIcon from "@mui/icons-material/HomeOutlined"
-import BookIcon from "@mui/icons-material/BookOutlined"
-import CallIcon from "@mui/icons-material/CallOutlined"
-import HandshakeIcon from "@mui/icons-material/HandshakeOutlined"
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
-import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined"
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined"
-import logo from "../../assets/logo.png"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import HomeIcon from "@mui/icons-material/HomeOutlined";
+import BookIcon from "@mui/icons-material/BookOutlined";
+import CallIcon from "@mui/icons-material/CallOutlined";
+import HandshakeIcon from "@mui/icons-material/HandshakeOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <section className="navbar">
-      <div className="logo">
-        {/* <img src={logo} className='logo-img' alt='logo' /> */}
-        <h1>Franchizerz</h1>
-        <h4>Your Own Partner</h4>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src={logo} className={styles.logoImg} alt="Franchizerz Logo" />
+        <div className={styles.logoText}>
+          <h1 className={styles.logoH1}>Franchizerz</h1>
+          <h4 className={styles.logoH4}>Your Own Partner</h4>
+        </div>
       </div>
 
-      <ul>
-        <li>
-          <Link to="/">
-            <HomeIcon /> Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/blogs">
-            <BookIcon />
-            Blogs
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact-us">
-            <CallIcon />
-            Contact Us
-          </Link>
-        </li>
-        <li>
-          <Link to="/partners">
-            <HandshakeIcon />
-            Partners
-          </Link>
-        </li>
-      </ul>
-
-      <div className="search-box">
-        <input type="text" placeholder="Search" />
-        <SearchOutlinedIcon />
+      <div className={styles.mobileMenuIcon} onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
       </div>
 
-      <ul>
-        <li>
-          <Link to="/login">
-            <LoginOutlinedIcon />
-            Log In
-          </Link>
-        </li>
-        <li>
-          <Link to="/wishlist">
-            <FavoriteBorderOutlinedIcon />
-            WishList
-          </Link>
-        </li>
-      </ul>
-    </section>
-  )
-}
+      <div className={`${styles.navLinks} ${isMobileMenuOpen ? styles.navLinksActive : ""}`}>
+        <ul className={styles.navLinksUl}>
+          <li className={styles.navLinksLi}>
+            <Link to="/" className={styles.navLinksA}>
+              <HomeIcon className={styles.navLinksSvg} /> Home
+            </Link>
+          </li>
+          <li className={styles.navLinksLi}>
+            <Link to="/blogs" className={styles.navLinksA}>
+              <BookIcon className={styles.navLinksSvg} /> Blogs
+            </Link>
+          </li>
+          <li className={styles.navLinksLi}>
+            <Link to="/contact-us" className={styles.navLinksA}>
+              <CallIcon className={styles.navLinksSvg} /> Contact Us
+            </Link>
+          </li>
+          <li className={styles.navLinksLi}>
+            <Link to="/partners" className={styles.navLinksA}>
+              <HandshakeIcon className={styles.navLinksSvg} /> Partners
+            </Link>
+          </li>
+          <li className={styles.navLinksLi}>
+            <Link to="/login" className={styles.navLinksA}>
+              <AccountCircleOutlinedIcon className={styles.navLinksSvg} /> Log In
+            </Link>
+          </li>
+          <li className={styles.navLinksLi}>
+            <Link to="/wishlist" className={styles.navLinksA}>
+              <FavoriteBorderOutlinedIcon className={styles.navLinksSvg} /> WishList
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-export default Navbar
+      <div className={styles.searchBox}>
+        <input type="text" placeholder="Search" className={styles.searchBoxInput} />
+        <SearchOutlinedIcon className={styles.searchBoxSvg} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
